3d9a160505303dc1fa5feb527d2567f606da52a6
Arlind_Frakulla <arlfra17@htl-shkoder.com>
2023-04-28 01:34:56 +0200

    adding move rules

--- lib/screens/games/chess/chess_logic.dart
+++ lib/screens/games/chess/chess_logic.dart
@@ -1,31 +1,108 @@
+import 'package:game_template/screens/games/chess/piece_logic.dart';
 
-class ChessLogic{
+class GameState{
+  late bool isWhiteTurn;
+  late bool blackQueenSide;
+  late bool blackKingSide;
+  late bool whiteQueenSide;
+  late bool whiteKingSide;
+  Piece? lastEnPassantMove;
+  List<Piece> gamePieces = [];
+  late int halfMovesFromCoPM;
+  late int totalFullMoves;
+
+  bool inCheckWhite = false;
+  bool inCheckBlack = false;
+
   final String defaultStartingFen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
   ChessLogic(){
-    fenParser(defaultStartingFen);
+    fenParser(defaultStartingFen, initialise: true);
   }
-  fenParser(String fen){
+  fenParser(String fen,{bool initialise = false}){
     List<String> rules = fen.split(" ");
     if(rules.length == 6){
       String piecePlacement = fen[0];
       String nextPlayerMove = fen[1];
+      assert(nextPlayerMove == "w" || nextPlayerMove == "b" );
       String castlingRights = fen[2];
+      assert(castlingRights.contains(RegExp("(k|q|K|q|-)")));
       String enPassantPlayed = fen[3];
+      assert(enPassantPlayed.contains(RegExp("([a-h][0-8]|-)")));
       String halfMove = fen[4];
+      assert(int.tryParse(halfMove)!=null);
       String fullMove = fen[5];
+      assert(int.tryParse(fullMove)!=null);
+
+      if(initialise){
+        isWhiteTurn = nextPlayerMove == "w";
+        whiteKingSide = castlingRights.contains("K");
+        whiteQueenSide = castlingRights.contains("Q");
+        blackKingSide = castlingRights.contains("k");
+        blackQueenSide = castlingRights.contains("q");
+        halfMovesFromCoPM = int.parse(halfMove);
+        totalFullMoves = int.parse(fullMove);
+      }
+      fenPieceParser(piecePlacement, initialise: initialise);
     }else{
       throw "fan input is not correct is it doesn't feature all 6 rules";
     }
   }
 
-  fenPieceParser(String fenPieces){
+  fenPieceParser(String fenPieces, {bool initialise = false}){
     List<String> fenPieceInRanks = fenPieces.split("/");
+    List<Piece> pieces = [];
     if(fenPieceInRanks.length == 8){
-
+      int rank = 1;
+      for(int i = 0 ; i < fenPieceInRanks.length; i++){
+        rank = i+1;
+        int file = 1;
+        for(int j = 0; j < fenPieceInRanks[i].length; j++) {
+          assert(fenPieceInRanks[i][j].contains("(r|b|n|k|q|p|R|B|N|K|Q|P|[0-8])"));
+          int? emptySpaces = int.tryParse(fenPieceInRanks[i][j]);
+          if(emptySpaces == null){
+            bool isWhite = fenPieceInRanks[i][j].toUpperCase() == fenPieceInRanks[i][j] && fenPieceInRanks[i][j].isNotEmpty;
+            switch(fenPieceInRanks[i][j].toUpperCase()){
+              case "R":
+                pieces.add(Rook(location: Location(rank: rank, file:  file), isWhite: isWhite));
+                file++;
+                break;
+              case "B":
+                pieces.add(Bishop(location: Location(rank: rank, file:  file), isWhite: isWhite));
+                file++;
+                break;
+              case "N":
+                pieces.add(Knight(location: Location(rank: rank, file:  file), isWhite: isWhite));
+                file++;
+                break;
+              case "K":
+                pieces.add(King(location: Location(rank: rank, file:  file), isWhite: isWhite));
+                file++;
+                break;
+              case "Q":
+                pieces.add(Queen(location: Location(rank: rank, file:  file), isWhite: isWhite));
+                file++;
+                break;
+              case "P":
+                pieces.add(Pawn(location: Location(rank: rank, file:  file), isWhite: isWhite));
+                file++;
+                break;
+              default:
+                assert(false);
+                break;
+            }
+          }else{
+            file += emptySpaces;
+          }
+        }
+      }
     }else{
       throw "fan Piece input is not correct or it doesn't feature 8 ranks";
     }
   }
+  toFen(){
 
+  }
 }
+
+
 
--- lib/screens/games/chess/piece_logic.dart
+++ lib/screens/games/chess/piece_logic.dart
@@ -1,35 +1,106 @@
-class PieceLogic{
-  bool isWhiteTurn = true;
-  bool inCheckWhite = false;
-  bool inCheckBlack = false;
-  bool blackQueenSide = true;
-  bool blackKingSide = true;
-  bool whiteQueenSide = true;
-  bool whiteKingSide = true;
-  Piece? lastEnPassantMove;
-}
+import 'dart:collection';
 
+class Location extends LinkedListEntry<Location>{
+  int rank;
+  int file;
+  bool get inside => (rank >= 1 && rank <= 8) && (file >= 1 && file <= 8);
+  String get nameConvention => inside ? String.fromCharCode(96 + file) + rank.toString() : "outside";
+
+  Location({
+    required this.rank,
+    required this.file,
+  });
+}
 
 class Piece{
   bool isWhite;
   bool eaten;
-  int rank;
-  int file;
-  String get nameConvention => (rank >= 1 && rank <= 8) && (file >= 1 && file <= 8) ? String.fromCharCode(96 + file) + rank.toString() : "eaten";
+  Location location;
   Piece({
-    required this.rank,
-    required this.file,
+    required this.location,
     required this.isWhite,
     this.eaten = false,
   });
+
+
 }
 
-class Pawn extends Piece{
+class Rook extends Piece{
   int doubleMoveAllowed;
-  Pawn({
-    required super.rank,
-    required super.file,
+  List<LinkedList<Location>> get pieceBySelfAllMoves {
+    List<LinkedList<Location>> res = [];
+    return res;
+  }
+  Rook({
+    required super.location,
     required super.isWhite,
     super.eaten = false,
   }): doubleMoveAllowed = isWhite ? 2 : 7;
+
+}
+
+class Bishop extends Piece{
+  int doubleMoveAllowed;
+  List<LinkedList<Location>> get pieceBySelfAllMoves {
+    List<LinkedList<Location>> res = [];
+    return res;
+  }
+  Bishop({
+    required super.location,
+    required super.isWhite,
+    super.eaten = false,
+  }): doubleMoveAllowed = isWhite ? 2 : 7;
+
+}
+class Knight extends Piece{
+  int doubleMoveAllowed;
+  List<LinkedList<Location>> get pieceBySelfAllMoves {
+    List<LinkedList<Location>> res = [];
+    return res;
+  }
+  Knight({
+    required super.location,
+    required super.isWhite,
+    super.eaten = false,
+  }): doubleMoveAllowed = isWhite ? 2 : 7;
+
+}
+class King extends Piece{
+  int doubleMoveAllowed;
+  List<LinkedList<Location>> get pieceBySelfAllMoves {
+    List<LinkedList<Location>> res = [];
+    return res;
+  }
+  King({
+    required super.location,
+    required super.isWhite,
+    super.eaten = false,
+  }): doubleMoveAllowed = isWhite ? 2 : 7;
+
+}
+class Queen extends Piece{
+  int doubleMoveAllowed;
+  List<LinkedList<Location>> get pieceBySelfAllMoves {
+    List<LinkedList<Location>> res = [];
+    return res;
+  }
+  Queen({
+    required super.location,
+    required super.isWhite,
+    super.eaten = false,
+  }): doubleMoveAllowed = isWhite ? 2 : 7;
+
+}
+class Pawn extends Piece{
+  int doubleMoveAllowed;
+  List<LinkedList<Location>> get pieceBySelfAllMoves {
+    List<LinkedList<Location>> res = [];
+    return res;
+  }
+  Pawn({
+    required super.location,
+    required super.isWhite,
+    super.eaten = false,
+  }): doubleMoveAllowed = isWhite ? 2 : 7;
+
 }
