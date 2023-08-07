329b561afe092b7041e84ac7c9a7a28b1e5e99cb
Arlind Frakulla <real.estate.app.22@gmail.com>
2023-08-07 13:49:15 +0200

    account changes

--- lib/screens/account/account_authenticated.dart
+++ lib/screens/account/account_authenticated.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:game_template/services/firebase/firebase_auth.dart';
+import 'package:game_template/services/get_it_helper.dart';
+import 'package:game_template/widgets/button_widget.dart';
+import 'package:game_template/widgets/text_widget.dart';
+
+import '../screens_controller.dart';
 
 class AccountAuthenticated extends StatelessWidget {
   const AccountAuthenticated({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        ButtonWidget(
+          textWidget: TextWidget(
+            text: "Sign out"
+          ),
+          action: () {
+            getIt<FirebaseAuthUser>().logOut().then((value){
+              getIt<ScreensController>().notifyListeners();
+            });
+          },
+        )
+      ],
+    );
   }
 }
